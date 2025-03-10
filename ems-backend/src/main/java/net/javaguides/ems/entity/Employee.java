a247a3bd2f57231936be82d609a8cfc4ceaca247
BESHIN <buiminhhieu4241@gmail.com>
2025-03-10 12:02:39 +0700

    first commit

---
+++ ems-backend/src/main/java/net/javaguides/ems/entity/Employee.java
@@ -0,0 +1,28 @@
+package net.javaguides.ems.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private int email;
+
+}
