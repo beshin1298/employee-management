8791aa3d53e6b59052199f73d65bf811adc874f1
BESHIN <buiminhhieu4241@gmail.com>
2025-03-10 12:32:33 +0700

    backend: add mapstruct and employee mapper

---
+++ ems-backend/src/main/java/net/javaguides/ems/mapper/EmployeeMapper.java
@@ -0,0 +1,17 @@
+package net.javaguides.ems.mapper;
+
+import net.javaguides.ems.dto.EmployeeDTO;
+import net.javaguides.ems.entity.Employee;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
+public interface EmployeeMapper {
+    EmployeeDTO toDTO(Employee employee);
+    Employee toEntity(Employee employeeDTO);
+    List<EmployeeDTO> toDTOList(List<Employee> employees);
+    List<Employee> toEntityList(List<EmployeeDTO> employeeDTOs);
+}
